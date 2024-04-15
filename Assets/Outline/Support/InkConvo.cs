62b6f1b3524e92277da8d71b8e3c6a2ba3d0e953
luckylooser2013 <arodriguezr2013@gmail.com>
2024-04-15 13:07:35 -0500

    Fixed the Ink Related scripts.
    It works now, though the InkConvo Script might be obsolete now.

--- Assets/Outline/Support/InkConvo.cs
+++ Assets/Outline/Support/InkConvo.cs
@@ -9,31 +9,13 @@ using Ink.Runtime;
 public class InkConvo : MonoBehaviour
 {
     public TextAsset InkStory;
-    public TextMeshProUGUI textBox;
-    public Story story;
-    public Canvas canvas;
 
     public void Awake()
     {
         InkManager.self.StartInkConvo(InkStory);
-        story = new Story(InkStory.text);
-    }
-    public void RunInk()
-    {     
         
-        while (story.canContinue)
-        {
-            string text = story.Continue();
-            text.Trim();
-            ContentView(text);
-        }
-
     }
+   
 
-    void ContentView(string text)
-    {
-        TextMeshProUGUI storyText = Instantiate(textBox);
-        storyText.text = text;
-        storyText.transform.SetParent(canvas.transform, false);
-    }
+    
 }
