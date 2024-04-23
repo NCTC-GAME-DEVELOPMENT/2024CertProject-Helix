0f89c8483f02061189aa071128ba148c00861121
luckylooser2013 <arodriguezr2013@gmail.com>
2024-04-23 18:41:43 -0500

    Re-Pushed the Ink Manager
    Re-pushed the upgraded InkManager script.

---
+++ Assets/Scripts/InkManager.cs
@@ -0,0 +1,129 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Ink.Runtime;
+using TMPro;
+using System.Linq;
+using Unity.VisualScripting;
+using UnityEngine.EventSystems;
+
+public class InkManager : MonoBehaviour
+{
+    public GameObject dialoguePanel;
+    public bool isDialoguePlaying;
+    public bool canContinueStory;
+    public static InkManager self;
+    public Story story;
+    public TextMeshProUGUI textBox;
+    public TextAsset InkStory;
+    public GameObject[] choices;
+    public TextMeshProUGUI[] choicesText;
+
+
+    private void Awake()
+    {
+     
+        if (self != null)
+        {
+            Debug.LogError("There's more than one InkManager in the scene. " +gameObject.name);
+        }
+        self = this;
+    }
+    
+    void Start()
+    {
+        isDialoguePlaying = false;
+        dialoguePanel.SetActive(false);
+
+        choicesText = new TextMeshProUGUI[choices.Length];
+        int index = 0;
+        foreach (GameObject choice in choices)
+        {
+            choicesText[index] = choice.GetComponentInChildren<TextMeshProUGUI>();
+            index++;
+        }
+    }
+
+    void Update()
+    {
+        if(!isDialoguePlaying)
+        {
+            return;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            ContinueStory();
+        }
+    }
+
+   public void ExitInkConvo()
+   {
+        isDialoguePlaying = false;
+        dialoguePanel.SetActive(false);
+        textBox.text = "";
+   }
+
+    public void StartInkConvo(TextAsset InkFileName)
+    {
+        Debug.Log("Manager Is starting inkfile");
+        story = new Story(InkFileName.text);
+        isDialoguePlaying = true;
+        dialoguePanel.SetActive(true);
+        DisplayChoices();
+    }
+
+    private void ContinueStory()
+    {
+        if (story.canContinue)
+        {
+            canContinueStory = true;
+            textBox.text = story.Continue();
+            DisplayChoices();
+        }
+        else
+        {
+            ExitInkConvo();
+        }
+    }
+
+    private void DisplayChoices()
+    {
+        List<Choice> currentChoices = story.currentChoices;
+
+        if (currentChoices.Count > choices.Length)
+        {
+            Debug.LogError("Too many choices for the UI to support. Number of choices: " + currentChoices.Count);
+        }
+
+        int index = 0;
+        foreach (Choice choice in currentChoices)
+        {
+            choices[index].gameObject.SetActive(true);
+            choicesText[index].text = choice.text;
+            index++;
+        }
+        for (int i = index; i < choices.Length; i++) 
+        {
+            choices[i].gameObject.SetActive(false);
+        }
+
+        StartCoroutine(SelectFirstChoice());
+    }
+
+    private IEnumerator SelectFirstChoice()
+    {
+        EventSystem.current.SetSelectedGameObject(null);
+        yield return new WaitForEndOfFrame();
+        EventSystem.current.SetSelectedGameObject(choices[0].gameObject);
+    }
+
+    public void MakeChoice(int choiceIndex)
+    {
+        if (canContinueStory == true)
+        {
+            story.ChooseChoiceIndex(choiceIndex);
+            ContinueStory();
+        }
+    }
+}
